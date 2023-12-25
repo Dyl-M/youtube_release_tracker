0ed8d33f6a48e00f17fb61277be9efe7e7e00ff2
dyl-m <dylan.monfret.contact@gmail.com>
2023-12-25 19:57:48 +0100

    refactor: refactor unnecessary `else` / `elif` when `if` block has a `return` statement

--- src/main.py
+++ src/main.py
@@ -87,15 +87,11 @@ def dest_playlist(channel_id: str, is_shorts: bool, v_duration: int, max_duratio
         if v_duration > max_duration * 60:
             if channel_id in other:
                 return watch_later
-            else:
-                return 'none'
-
+            return 'none'
         if channel_id in favorites:
             return banger
-        else:
-            return release
-    else:
-        return watch_later
+        return release
+    return watch_later
 
 
 def update_repo_secrets(secret_name: str, new_value: str, logger: logging.Logger = None):
