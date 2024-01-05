a1f1febb625a4c9181b7d5f665964e89da894b67
dyl_m <dylan.monfret.contact@gmail.com>
2024-01-05 10:09:17 +0100

    chore: minor change

--- src/main.py
+++ src/main.py
@@ -23,9 +23,7 @@ try:
 
 except KeyError:
     github_repo = 'Dyl-M/auto_youtube_playlist'
-
-    with open('../tokens/TMP_PAT.txt', 'r', encoding='utf-8') as PAT_FILE:
-        PAT = PAT_FILE.readlines()[0]
+    PAT = 'PAT'
 
 "SYSTEM"
 
